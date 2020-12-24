b414f935ad9ede152cf27d376bbb0d4bbcbc1afb
Claire Bohlen <clairebohlen@gmail.com>
2020-12-24 07:23:07 -0700

    fixed bug

--- scripts.js
+++ scripts.js
@@ -1,22 +1,37 @@
 //Dom Elements
-const submit = document.querySelector('.submit')
-const symbolsEl = document.querySelector('.symbols-check')
-const numbersEl = document.querySelector('.numbers-check')
-const lowerEl = document.querySelector('.lower-check')
-const upperEl = document.querySelector('.upper-check')
-const lengthEl = document.querySelector('.length-check')
-const outputEl = document.querySelector('.output-area')
+const submit = document.querySelector('.submit');
+const symbolsEl = document.querySelector('.symbols-check');
+const numbersEl = document.querySelector('.numbers-check');
+const lowerEl = document.querySelector('.lower-check');
+const upperEl = document.querySelector('.upper-check');
+const lengthEl = document.querySelector('.length-check');
+const outputEl = document.querySelector('.output-area');
 //add clipboard
+
+// Randon Functions 
+function randomLowerCase (){
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 97)
+};
+
+function randomUppercase (){
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 65)
+};
+function randomNumber (){
+    return String.fromCharCode(Math.floor(Math.random() * 10) + 49)
+};
+function randomSymbol (){
+    return String.fromCharCode(Math.floor(Math.random() * 15) + 33)
+};
 
 //Put random functions into an object
 const randomFunc = {
     lower: randomLowerCase,
     upper: randomUppercase,
-    numer: randomNumber,
+    number: randomNumber,
     symbol: randomSymbol
     
     
-}
+};
 
 
 
@@ -29,62 +44,47 @@ submit.addEventListener('click', ()=> {
     const hasUpper = upperEl.checked;
     const hasSymbol = symbolsEl.checked;
     const hasNumber = numbersEl.checked;
-    // console.log(length)
-    // console.log(hasLower)
-    // console.log(hasUpper)
-    // console.log(hasNumber)
-    // console.log(hasSymbol)
+   
 
 
-    outputEl.innerText = generatePassword(hasLower, hasNumber, hasSymbol, hasUpper,length)
+    outputEl.innerText = generatePassword(hasLower, hasUpper, hasNumber, hasSymbol, length);
     
 
-})
+});
 
 //Generate the Final password based on the user input and checked boxes
 function generatePassword(lower, upper, number, symbol, length){
     let generatedPassword = '';
-    
-   //number of checked boxes 
-   //gets the object with true or false object 
     const typesCount = lower + upper + number + symbol ;
-    
-    
-    
-    
-     //Array of Objects by wrapping in curly braces
-    //If false it will be filtered out of the array
-    const typesArray = [{lower}, {upper}, {number}, {symbol}].filter(item => Object.values(item)[0])
+    const typesArray = [{lower}, {upper}, {number}, {symbol}].filter(item => Object.values(item)[0]);
    
+    console.log(typesArray)
     
     //If none are checked it will return nothing
     if(typesCount === 0){
         return ''
 
-    }
+    };
     
-    //Loop though the typesArray and call the 
-    for(let i=0; i<length; i+=typesCount) {
-        typesArray.forEach(type => {
-            const funcName = Object.keys(type)[0];
-            // console.log(funcName)
+   
 
-            generatedPassword += randomFunc[funcName]();
-            // generatedPassword += randomFunc[funcName]();
-            
-
-        })
-
-    }
+    for (let i = 0; i < length; i++) {
+		typesArray.forEach(type => {
+			const funcName = Object.keys(type)[0];
+			generatedPassword += randomFunc[funcName]();
+		});
+	}
+   
+    
     
 	
 
-    const finalPassword = generatedPassword.slice(0, length)
-    console.log(finalPassword)
+    const finalPassword = generatedPassword.slice(0, length);
+    // console.log(finalPassword)
     
     
 
-    return finalPassword
+    return finalPassword;
 
 }
 
@@ -92,27 +92,11 @@ function generatePassword(lower, upper, number, symbol, length){
 
 
 
-// Randon Functions 
-function randomLowerCase (){
-    return String.fromCharCode(Math.floor(Math.random() * 26) + 97)
-}
-
-function randomUppercase (){
-    return String.fromCharCode(Math.floor(Math.random() * 26) + 65)
-}
-function randomNumber (){
-    return String.fromCharCode(Math.floor(Math.random() * 10) + 49)
-}
-function randomSymbol (){
-    return String.fromCharCode(Math.floor(Math.random() * 15) + 33)
-}
 
 
 
-// console.log(randomUppercase())
-// console.log(randomLowerCase())
-// console.log(randomNumber())
-// console.log(randomSymbol())
+
+
 
 
 
